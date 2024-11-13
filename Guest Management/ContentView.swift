6935d7c161e34ecd0aa9b18f7c09ceb45f0b93dd
Fahmi Dzulqarnain <fahmi.dzulqarnain@silentmode.my>
2024-11-13 14:06:17 +0700

    Initial Commit

---
+++ Guest Management/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  Guest Management
+//
+//  Created by Fahmi Dzulqarnain on 13/11/2024.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ Guest Management/Guest_ManagementApp.swift
@@ -0,0 +1,17 @@
+//
+//  Guest_ManagementApp.swift
+//  Guest Management
+//
+//  Created by Fahmi Dzulqarnain on 13/11/2024.
+//
+
+import SwiftUI
+
+@main
+struct Guest_ManagementApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
